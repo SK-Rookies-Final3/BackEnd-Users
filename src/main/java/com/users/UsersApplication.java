f2ed506ebaa52e359fa4cb56add2f9879729490c
DESKTOP-304U2MR\r2com <thvldk1654@naver.com>
2024-11-19 11:01:24 +0900

    fix : 의존성 수정

--- src/main/java/com/users/UsersApplication.java
+++ src/main/java/com/users/UsersApplication.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UsersApplication {
 	public static void main(String[] args) {
