032b1fe87b7eb3973ff3cc903dba3867d6eeb9ae
DESKTOP-304U2MR\r2com <thvldk1654@naver.com>
2024-11-19 10:43:23 +0900

    fix : securityConfig 완료 전체 코드

--- src/main/java/com/users/common/config/SecurityConfig.java
+++ src/main/java/com/users/common/config/SecurityConfig.java
@@ -1,21 +1,31 @@
 package com.users.common.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
+
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("*"));
+                    config.setAllowedMethods(List.of("GET", "POST", "DELETE", "OPTIONS", "PATCH"));
+                    config.setAllowedHeaders(List.of("*"));
+                    config.setAllowCredentials(true);
+                    return config;
+                }))
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/update").authenticated()
-                        .anyRequest().permitAll()
+                .authorizeExchange(authz -> authz // 리액티브에서는 authorizeExchange 사용
+                        .pathMatchers("/update").authenticated()
+                        .anyExchange().permitAll()
                 );
         return http.build();
     }
