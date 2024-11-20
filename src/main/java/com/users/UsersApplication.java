65d965195c4d7b9c24c6fe5bf6961e55182b0257
DESKTOP-304U2MR\r2com <thvldk1654@naver.com>
2024-11-20 10:20:31 +0900

    fix : eureka X

--- src/main/java/com/users/UsersApplication.java
+++ src/main/java/com/users/UsersApplication.java
@@ -1,9 +1,9 @@
 package com.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class UsersApplication {
 	public static void main(String[] args) {
