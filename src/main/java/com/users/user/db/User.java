821a4eb3acfaebd34b80dfe9f3c22c888e4c3510
baeppsae <minjkim8898@gmail.com>
2024-11-22 15:21:33 +0900

    @GeneratedValue(strategy = GenerationType.IDENTITY) 수정

--- src/main/java/com/users/user/db/User.java
+++ src/main/java/com/users/user/db/User.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode(callSuper = false) // Lombok 경고 해결
 public class User  {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(nullable = false, unique = true)
     private String username;
@@ -45,5 +45,5 @@ public class User  {
     private LocalDateTime createdAt;
     @UpdateTimestamp
     @Column
-    private LocalDateTime LastAcceptedAt;
+    private LocalDateTime lastAcceptedAt;
 }
